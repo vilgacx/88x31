0fde9adcda989e41e70e80b3344b5defec4baab3
xorvet <cypherx702@gmail.com>
2023-05-05 00:02:27 +0530

    get files

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let files = {};
+let files = [];
 
 function main() {
   fetch("https://api.github.com/repos/xorvet/88x31/git/trees/da60b7df28f8dd45a64298cbc39e2a0821fb5632")
     .then((response) => response.json())
-    .then((data) => { files = data['tree'] })
-    .catch((error) => { console.log(error) });
+    .then((data) => {
+      (data['tree']).forEach((item) => {
+        files.push(item['path']);
+      });
+    }).catch((error) => {
+      console.log(error)
+    }); 
+  
 };main();
 
 function Search() {
   const SearchTerm = document.querySelector("input").value;
-  console.log(SearchTerm,files);
+  console.log(SearchTerm);
 }
