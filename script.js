5d98c89f3ef61331de267d9a2c8e73b0460fe26f
xorvet <cypherx702@gmail.com>
2023-05-04 23:53:13 +0530

    script update

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+let files = {};
+
+function main() {
+  fetch()
+    .then((response) => response.json())
+    .then((data) => { files = data })
+    .catch((error) => { console.log(error) });
+};main();
+
 function Search() {
   const SearchTerm = document.querySelector("input").value;
-  console.log(SearchTerm);
-  fetch('/gifs/')
-    .then(response => response.text())
-    .then((data) => {
-      console.log(data)
-    })
-    .catch(error => console.error(error));
+  console.log(SearchTerm,files);
 }
