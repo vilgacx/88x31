13bd544623de8fc664a3b49fd0012187058a64b6
xorvet <cypherx702@gmail.com>
2023-05-05 00:26:58 +0530

    display gofs

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function main() {
   fetch("https://api.github.com/repos/xorvet/88x31/git/trees/da60b7df28f8dd45a64298cbc39e2a0821fb5632")
     .then((response) => response.json())
     .then((data) => {
+      const container = document.getElementById('contain-gifs');
       (data['tree']).forEach((item) => {
         files.push(item['path']);
+        const img = document.createElement('img');
+        img.src = '/gifs/' + item['path'];
+        container.appendChild(img);
       });
     }).catch((error) => {
-      console.log(error)
+      console.log(error);
     }); 
   
 };main();
@@ -16,4 +20,5 @@ function main() {
 function Search() {
   const SearchTerm = document.querySelector("input").value;
   console.log(SearchTerm);
+
 }
