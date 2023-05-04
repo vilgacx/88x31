d14ee59f0cfc9cac3fb2a992da4425f513b9c39c
xorvet <cypherx702@gmail.com>
2023-05-04 23:55:22 +0530

    minor fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let files = {};
 function main() {
   fetch("https://api.github.com/repos/xorvet/88x31/git/trees/da60b7df28f8dd45a64298cbc39e2a0821fb5632")
     .then((response) => response.json())
-    .then((data) => { files = data })
+    .then((data) => { files = data['tree'] })
     .catch((error) => { console.log(error) });
 };main();
 
