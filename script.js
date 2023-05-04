af77fe7c83b61f1678a678fc63510b207e9eb713
xorvet <cypherx702@gmail.com>
2023-05-05 01:12:52 +0530

    major changes

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let files = [];
 
 function main() {
-  fetch("https://api.github.com/repos/xorvet/88x31/git/trees/6812e3bb73c335e246364147ef2cf4fd2f1ff9f2")
+  fetch("https://api.github.com/repos/xorvet/88x31/git/trees/main")
     .then((response) => response.json())
-    .then((data) => {
-      const container = document.getElementById('contain-gifs');
-      (data['tree']).forEach((item) => {
-        files.push(item['path']);
-        const img = document.createElement('img');
-        img.src = `${window.location.pathname}gifs/${item['path']}`;
-        container.appendChild(img);
-      });
+    .then((data) => data['tree'][1]['url'])
+    .then((url) => {
+      fetch(url)
+        .then((result) => result.json())
+        .then((data) => {
+          const container = document.getElementById('contain-gifs');
+          (data['tree']).forEach((item) => {
+            files.push(item['path']);
+            const img = document.createElement('img');
+            img.src = `${window.location.pathname}gifs/${item['path']}`;
+            container.appendChild(img);
+          });
+        }).catch((error) => {
+          console.log(error);
+        });
     }).catch((error) => {
       console.log(error);
-    }); 
-  
-};main();
+    })
+
+}; main();
 
 function Search() {
   const SearchTerm = document.querySelector("input").value;
