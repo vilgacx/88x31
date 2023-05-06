b57afacaaf071e1e022c633ec0e86756eca7f840
xorvet <cypherx702@gmail.com>
2023-05-07 02:14:47 +0530

    update

--- script.js
+++ script.js
@@ -2,31 +2,29 @@ document.querySelector("input").style.display = "initial";
 
 let files = [];
 
-function main() {
-  fetch("https://api.github.com/repos/xorvet/88x31/git/trees/main")
-    .then((response) => response.json())
-    .then((data) => data['tree'][1]['url'])
-    .then((url) => {
-      fetch(url)
-        .then((result) => result.json())
-        .then((data) => {
-          const container = document.getElementById('contain-gifs');
-          (data['tree']).forEach((item) => {
-            files.push(item['path']);
-            const img = document.createElement('img');
-            img.src = `${window.location.pathname}gifs/${item['path']}`;
-            container.appendChild(img);
-          });
-        }).catch((error) => {
-          console.log(error);
+fetch("https://api.github.com/repos/xorvet/88x31/git/trees/main")
+  .then((response) => response.json())
+  .then((data) => data['tree'][1]['url'])
+  .then((url) => {
+    fetch(url)
+      .then((result) => result.json())
+      .then((data) => {
+        const container = document.getElementById('contain-gifs');
+        (data['tree']).forEach((item) => {
+          files.push(item['path']);
+          const img = document.createElement('img');
+          img.src = `${window.location.pathname}gifs/${item['path']}`;
+          container.appendChild(img);
         });
-    }).catch((error) => {
-      console.log(error);
-    })
+      }).catch((error) => {
+        console.log(error);
+      });
+  }).catch((error) => {
+    console.log(error);
+  });
 
-}; main();
 
-function Search() { 
+function Search() {
   const SearchTerm = (document.querySelector("input").value).toLowerCase();
   const container = document.getElementById('contain-gifs');
   container.replaceChildren();
@@ -34,7 +32,7 @@ function Search() {
     if (item.includes(SearchTerm) === true) {
       const img = document.createElement('img');
       img.src = `${window.location.pathname}gifs/${item}`;
-      container.appendChild(img); 
-    }; 
+      container.appendChild(img);
+    };
   });
 }
