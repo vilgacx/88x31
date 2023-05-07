d3b1f3cae3de7df2974f4cb3e315b4eda101e5d2
xorvet <cypherx702@gmail.com>
2023-05-08 00:38:44 +0530

    redirect img + code quality improvment

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ document.querySelector("input").style.display = "initial";
 
 let files = [];
 
+function createGIF(name) {
+  const img = document.createElement('img');
+  const a = document.createElement('a'); 
+  img.src = `${window.location.pathname}gifs/${name}`;
+  a.href = `${window.location.pathname}gifs/${name}`;
+  a.target = '_blank';
+  a.appendChild(img);
+  return a;
+}
+
 fetch("https://api.github.com/repos/xorvet/88x31/git/trees/main")
   .then((response) => response.json())
   .then((data) => data['tree'][1]['url'])
@@ -12,9 +22,8 @@ fetch("https://api.github.com/repos/xorvet/88x31/git/trees/main")
         const container = document.getElementById('contain-gifs');
         (data['tree']).forEach((item) => {
           files.push(item['path']);
-          const img = document.createElement('img');
-          img.src = `${window.location.pathname}gifs/${item['path']}`;
-          container.appendChild(img);
+          const GIFtag = createGIF(item['path']);
+          container.appendChild(GIFtag);
         });
       }).catch((error) => {
         console.log(error);
@@ -23,16 +32,14 @@ fetch("https://api.github.com/repos/xorvet/88x31/git/trees/main")
     console.log(error);
   });
 
-
 function Search() {
   const SearchTerm = (document.querySelector("input").value).toLowerCase();
   const container = document.getElementById('contain-gifs');
   container.replaceChildren();
   files.forEach((item) => {
     if (item.includes(SearchTerm) === true) {
-      const img = document.createElement('img');
-      img.src = `${window.location.pathname}gifs/${item}`;
-      container.appendChild(img);
+      const GIFtag = createGIF(item);
+      container.appendChild(GIFtag);
     };
   });
 }
